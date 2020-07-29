15d40293c375ca707fbf1007aed4e30c2c02a6c7
Dom King <dminicking91@gmail.com>
2020-07-29 16:40:23 +0100

    created the template design for the BMI calculator ready for advanced widget features

---
+++ lib/input_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(colour: activeCardColour)),
+                Expanded(child: ReusableCard(colour: activeCardColour)),
+          ])),
+          Expanded(child: ReusableCard(colour: activeCardColour)),
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: ReusableCard(colour: activeCardColour)),
+              Expanded(child: ReusableCard(colour: activeCardColour))
+          ])),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+
+  // final value, can only set the colour once! - final Color colour;
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
