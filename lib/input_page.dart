16348ee7b905ec1437f4cb1e5f42b3941443e617
Dom King <dminicking91@gmail.com>
2020-07-30 11:42:25 +0100

    updated the app to recognise gestures and colour control

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
 const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
@@ -10,6 +14,29 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  //1 = male, 2 = female
+  void updateColour(int gender){
+    if(gender == 1){
+      if(maleCardColour == inactiveCardColour) {
+        maleCardColour = activeCardColour;
+        femaleCardColour = inactiveCardColour;
+      } else {
+        maleCardColour = inactiveCardColour;
+      }
+    } else {
+      if(femaleCardColour == inactiveCardColour) {
+        femaleCardColour = activeCardColour;
+        maleCardColour = inactiveCardColour;
+      } else {
+        femaleCardColour = inactiveCardColour;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,14 +47,50 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(colour: activeCardColour)),
-                Expanded(child: ReusableCard(colour: activeCardColour)),
+                Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColour(1);
+                        });
+                      },
+                      child: ReusableCard(
+                          colour: maleCardColour,
+                          cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
+                      ),
+                    )
+                ),
+                Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColour(2);
+                        });
+                      },
+                      child: ReusableCard(
+                          colour: femaleCardColour,
+                          cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
+                      ),
+                    )
+                ),
           ])),
-          Expanded(child: ReusableCard(colour: activeCardColour)),
+          Expanded(
+              child: ReusableCard(
+                  colour: activeCardColour
+              )
+          ),
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReusableCard(colour: activeCardColour)),
-              Expanded(child: ReusableCard(colour: activeCardColour))
+              Expanded(
+                  child: ReusableCard(
+                      colour: activeCardColour
+                  )
+              ),
+              Expanded(
+                  child: ReusableCard(
+                      colour: activeCardColour
+                  )
+              )
           ])),
           Container(
             color: bottomContainerColour,
@@ -36,24 +99,6 @@ class _InputPageState extends State<InputPage> {
             height: bottomContainerHeight
           )
         ],
-      ),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
-
-  // final value, can only set the colour once! - final Color colour;
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
